44bd52b5d00f74f4f45318520351a7c741a161fc
Jingchen Xu <joel.jc.xu@gmail.com>
2017-11-29 19:05:01 -0500

    Implement illumination for point light source
    Add shading implementation for point light source using Phong shading.
    Completes task (d) for Part A.

--- raytracer/light_source.cpp
+++ raytracer/light_source.cpp
@@ -11,14 +11,42 @@
 #include <cmath>
 #include "light_source.h"
 
+/**
+ * Shading code for a view ray lit by a point light. Uses Phong shading to
+ * produce a colour based on intersection parameters. Assumes that
+ * ray.intersection.none == false and intersection information is available.
+ */
 void PointLight::shade( Ray3D& ray ) {
-	// TODO: implement this function to fill in values for ray.col 
-	// using phong shading.  Make sure your vectors are normalized, and
-	// clamp colour values to 1.0.
-	//
-	// It is assumed at this point that the intersection information in ray 
-	// is available.  So be sure that traverseScene() is called on the ray 
-	// before this function.  
 
+	// compute unit vector from intersection point to light source
+	Vector3D lightDir = _pos - ray.intersection.point;
+	lightDir.normalize();
+
+	// compute unit normal vector
+	Vector3D normalDir = ray.intersection.normal;
+	normalDir.normalize();
+
+	// compute unit vector from intersection point to camera
+	Vector3D viewDir = -1 * ray.dir;
+	viewDir.normalize();
+
+	// compute unit vector of the reflected light ray
+	Vector3D reflDir = ((2 * lightDir.dot(normalDir)) * normalDir) - lightDir;
+
+	// compute Phong intensities
+	double i_ambient = 1.0;
+	double i_diffuse = normalDir.dot(lightDir);
+	i_diffuse = i_diffuse < 0 ? 0 : i_diffuse;
+	double i_specular = reflDir.dot(viewDir);
+	i_specular = i_specular < 0 ? 0 : i_specular;
+	i_specular = pow(i_specular, ray.intersection.mat->specular_exp);
+
+	// compute component colours based on light and material colours
+	Colour c_ambient = _col_ambient * ray.intersection.mat->ambient;
+	Colour c_diffuse = _col_diffuse * ray.intersection.mat->diffuse;
+	Colour c_specular = _col_specular * ray.intersection.mat->specular;
+
+	// compute final pixel color as sum of components and set ray colour
+	ray.col = i_ambient * c_ambient + i_diffuse * c_diffuse + i_specular * c_specular;
+	ray.col.clamp(); // clamp colour values in range [0.0, 1.0]
 }
-
